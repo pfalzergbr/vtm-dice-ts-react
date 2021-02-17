156b8561f1c1cbc82bdc4a20c7d1a135a7f804b4
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-17 14:34:38 +0000

    test generateRoll

--- src/diceEngine/__tests__/diceEngine.test.ts
+++ src/diceEngine/__tests__/diceEngine.test.ts
@@ -1,5 +1,39 @@
-import {generateRoll} from '../diceEngine'
+import {generateRoll, regularDiceMap, hungerDiceMap} from '../diceEngine'
 
-test('should ', () => {
+
+describe('generateRoll', () => {
+  describe('When generating a roll of regular dice', () => {
+    const diceNum = 5
+    const diceMap = regularDiceMap
+    const type = 'regular'
+    const rollResult = generateRoll(diceNum, diceMap, type)
+
+    test('should return the correct number of result dice', () => {
+      expect(rollResult.length).toBe(diceNum)
+    })
+    
+    test('should return the array with correct types of dice', () => {
+      expect(rollResult.every(dice => dice.type === type )).toBe(true);
+    })
+  })
+  
+  describe('When generating a roll of hunger dice', () => {
+    const diceNum = 3
+    const diceMap = hungerDiceMap
+    const type = 'hunger'
+    const rollResult = generateRoll(diceNum, diceMap, type)
+
+    test('should return the correct number of result dice', () => {
+      expect(rollResult.length).toBe(diceNum)
+    })
+    
+    test('should return the array with correct types of dice', () => {
+      expect(rollResult.every(dice => dice.type === type )).toBe(true);
+    })
+  })
+
+
+  
+  
   
 })
