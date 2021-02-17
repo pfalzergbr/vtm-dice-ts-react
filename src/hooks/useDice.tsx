afaa9d107873aa4d3b00a479ce25bade4f078a1f
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-17 09:24:43 +0000

    add logic to rollDice

--- src/hooks/useDice.tsx
+++ src/hooks/useDice.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
 type RollResult = 'criticalSuccess' | 'criticalFail' | 'success' | 'fail';
+type FinalResult =
+  | 'criticalSuccess'
+  | 'bestialFail'
+  | 'messyCritical'
+  | 'success'
+  | 'fail';
 
 type DiceMap = [
   RollResult,
@@ -12,12 +18,17 @@ type DiceMap = [
   RollResult,
   RollResult,
   RollResult,
-  RollResult,
-]
+  RollResult
+];
 
 export interface Dice {
   value: number;
   result: RollResult;
+}
+
+export interface ResultMessage {
+  result: FinalResult;
+  message: string;
 }
 
 export const regularDiceMap: DiceMap = [
@@ -44,38 +55,44 @@ export const hungerDiceMap: DiceMap = [
   'success',
   'success',
   'success',
-]
+];
 
 export const useDice = (): {
   generateRandomDice: (diceMap: DiceMap) => Dice;
+  rollResult: Dice[];
 } => {
-  const [regularDicePool, setRegularDicePool] = useState<Dice[]>([]);
-  const [hungerDicePool, setHungerDicePool] = useState<Dice[]>([]);
+  // const [regularDicePool, setRegularDicePool] = useState<Dice[]>([]);
+  // const [hungerDicePool, setHungerDicePool] = useState<Dice[]>([]);
   const [rollResult, setRollResult] = useState<Dice[]>([]);
   const [resultDescription, setResultDescription] = useState(null);
 
   const generateRandomDice = (diceMap: DiceMap): Dice => {
     const value = Math.floor(Math.random() * 10);
     const result = diceMap[value];
-
-    return {
-      value,
-      result
-    };
+    return { value, result };
   };
 
-  const generateRoll = (diceNum: number) => {
-    // let rollArray: number[]
-    // for ( let i = 0; i < diceNum; i++){
-    //   const rolledNumber = generateRandomDice()
-    // }
-  };
+  const generateRoll = (diceNum: number, diceMap: DiceMap): Dice[] => {
+    const rollArray: Dice[] = [];
 
-  const createMessage = () => {};
+    for (let i = 0; i < diceNum; i++) {
+      const rolledDice = generateRandomDice(diceMap);
+      rollArray.push(rolledDice);
+    }
+
+    return rollArray;
+  };
 
   const evaluateRoll = () => {};
 
-  const rollDice = (normalDiceNum: number, hungerDiceNum: number) => {};
+  const createMessage = () => {};
 
-  return { generateRandomDice };
+  const rollDice = (regularDiceNum: number, hungerDiceNum: number) => {
+    const regularRoll = generateRoll(regularDiceNum, regularDiceMap);
+    const hungerRoll = generateRoll(hungerDiceNum, hungerDiceMap);
+    const rollResult = [...regularRoll, ...hungerRoll];
+    setRollResult(rollResult);
+  };
+
+  return { generateRandomDice, rollResult };
 };
